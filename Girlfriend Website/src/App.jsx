d581bbc73e42b416dacb2bc02f12b3a93f2123f7
brownmattthew <mattybrown349@hotmail.co.uk>
2025-06-22 17:10:13 +0100

    Password checker and some general CSS added

---
+++ Girlfriend Website/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Enter the password</h1>
+      <form>
+        <label htmlFor="password"></label>
+        <input type="password" id='password'/>
+      </form>
+    </>
+  )
+}
+
+export default App
