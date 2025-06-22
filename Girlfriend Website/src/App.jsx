f4de358e00dc89a166824714fb2a44c3d7a249e4
brownmattthew <mattybrown349@hotmail.co.uk>
2025-06-22 23:47:06 +0100

    Nearly finished the opening password screen going to look into making custom alerts

--- Girlfriend Website/src/App.jsx
+++ Girlfriend Website/src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+  function passwordCheck() {
+    const correctPassword = "daisy";
+    const enteredPassword = document.getElementById("password").value;
+
+    if(correctPassword != enteredPassword) {
+      alert("Password is incorrect!");
+    } else if (correctPassword == enteredPassword){
+      alert("Password is correct!")
+    } else {
+      alert("Please enter a valid password")
+    }
+  }
+
   return (
     <>
+    <div id='container'>
       <h1>Enter the password</h1>
       <form>
         <label htmlFor="password"></label>
-        <input type="password" id='password'/>
+        <input placeholder = "Enter Password" type="password" id='password'/>
+        <button id="passwordCheck" onClick={passwordCheck}> Go! </button>
       </form>
+    </div>
     </>
   )
 }
