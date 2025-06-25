4659e8d361388a34022352677b6f4276770c3615
brownmattthew <mattybrown349@hotmail.co.uk>
2025-06-26 00:05:14 +0100

    Added phone screen compatability to the home screen

--- Girlfriend Website/src/components/coolButton.jsx
+++ Girlfriend Website/src/components/coolButton.jsx
@@ -1,8 +1,8 @@
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function CoolButton({ image, title }) {
   return (
-    <div id="button" onClick={() => Navigate(to)}>
+    <div id="button">
       <a href={Link}>
         <img id="buttonImage" src={image} alt={title} />
         <h1 id="buttonTitle"> {title} </h1>
